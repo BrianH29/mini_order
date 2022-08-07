043724200149ee3ab0681654ee38c71609042b14
Brian Hwang <brianhwang29@gmail.com>
2022-08-07 17:49:04 +0900

    modified

--- deploy.sh
+++ deploy.sh
@@ -17,10 +17,7 @@ git checkout -b gh-pages
 git add -A
 git commit -m 'deploy'
 
-# if you are deploying to https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
-
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
- git push -f git@github.com:BrianH29/mini_order.git main:gh-pages
+git push -f git@github.com:BrianH29/mini_order.git main:gh-pages
 
 cd -
